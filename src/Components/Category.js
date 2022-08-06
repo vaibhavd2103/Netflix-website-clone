59837dc07b2776f27531076acdd762d54c1c8322
vaibhad2103 <bountydange@gmail.com>
2022-08-06 15:52:03 +0530

    checking for update

--- src/Components/Category.js
+++ src/Components/Category.js
@@ -7,115 +7,118 @@ import "../Constants/cssStyles.css";
 // import { API_KEY } from "../requests";
 
 function Category({ title, fetchURL, setDetail }) {
-  const [hover, setHover] = useState(false);
+	const [hover, setHover] = useState(false);
 
-  const [loading, setLoading] = useState(true);
-  const imageUrl = "https://image.tmdb.org/t/p/original/";
+	const [loading, setLoading] = useState(true);
+	const imageUrl = "https://image.tmdb.org/t/p/original/";
 
-  const [movies, setMovies] = useState([]);
+	const [movies, setMovies] = useState([]);
 
-  useEffect(() => {
-    const fetchMovies = async () => {
-      instance.get(fetchURL).then((response) => {
-        //    console.log(response.data.results);
-        setMovies(response.data.results);
-        setLoading(false);
-      });
-    };
-    fetchMovies();
-  }, []);
+	useEffect(() => {
+		const fetchMovies = async () => {
+			instance.get(fetchURL).then((response) => {
+				//    console.log(response.data.results);
+				setMovies(response.data.results);
+				setLoading(false);
+			});
+		};
+		fetchMovies();
+	}, []);
 
-  //   const fetchMovie = async (id) => {
-  //     await instance
-  //       .get(`/movie/${id}?api_key=1bbd459c320f161b8dee93104cf1740e`)
-  //       .then((response) => {
-  //         console.log(response.data);
-  //         setMovieData(response.data);
-  //         //    console.log(movieData);
-  //       });
-  //     localStorage.setItem("id", id);
-  //   };
+	//   const fetchMovie = async (id) => {
+	//     await instance
+	//       .get(`/movie/${id}?api_key=1bbd459c320f161b8dee93104cf1740e`)
+	//       .then((response) => {
+	//         console.log(response.data);
+	//         setMovieData(response.data);
+	//         //    console.log(movieData);
+	//       });
+	//     localStorage.setItem("id", id);
+	//   };
 
-  return (
-    <>
-      {loading ? (
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            width: "100vw",
-            height: "200",
-          }}
-        >
-          {/* <Loader
+	return (
+		<>
+			{loading ? (
+				<div
+					style={{
+						display: "flex",
+						justifyContent: "center",
+						width: "100vw",
+						height: "200",
+					}}
+				>
+					{/* <Loader
             type="Circles"
             color={colors.primary}
             height={40}
             width={40}
           /> */}
-        </div>
-      ) : (
-        <div
-          className="category_div"
-          style={{
-            display: "flex",
-            paddingLeft: 30,
-            flexDirection: "column",
-          }}
-        >
-          <a
-            className="category_name"
-            style={{
-              fontWeight: "600",
-              fontSize: 20,
-              zIndex: 10,
-              textShadow: 10,
-              marginLeft: 20,
-            }}
-          >
-            {title}
-          </a>
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              paddingTop: 20,
-              paddingBottom: 70,
-              paddingLeft: 20,
-            }}
-          >
-            {movies.map((item, i) => {
-              return (
-                <div
-                  onClick={() => {
-                    // fetchMovie(item.id);
-                    setDetail(true);
-                    localStorage.setItem("id", item.id);
-                  }}
-                  onMouseOver={() => {
-                    setHover(true);
-                  }}
-                  onMouseOut={() => {
-                    setHover(false);
-                  }}
-                  key={item.id}
-                  className="movie_block"
-                  style={{
-                    ...styles.movieBlock,
-                  }}
-                >
-                  <img
-                    src={`${imageUrl}${item.backdrop_path}`}
-                    className="category_poster"
-                  />
-                </div>
-              );
-            })}
-          </div>
-        </div>
-      )}
-    </>
-  );
+				</div>
+			) : (
+				<div
+					className="category_div"
+					style={{
+						display: "flex",
+						paddingLeft: 30,
+						flexDirection: "column",
+					}}
+				>
+					<a
+						className="category_name"
+						style={{
+							fontWeight: "600",
+							fontSize: 20,
+							zIndex: 10,
+							textShadow: 10,
+							marginLeft: 20,
+						}}
+					>
+						{title}
+					</a>
+					<div
+						style={{
+							display: "flex",
+							flexDirection: "row",
+							paddingTop: 20,
+							paddingBottom: 70,
+							paddingLeft: 20,
+							width: "100%",
+							// overflowX: "scroll",
+							zIndex: 20,
+						}}
+					>
+						{movies.map((item, i) => {
+							return (
+								<div
+									onClick={() => {
+										// fetchMovie(item.id);
+										setDetail(true);
+										localStorage.setItem("id", item.id);
+									}}
+									onMouseOver={() => {
+										setHover(true);
+									}}
+									onMouseOut={() => {
+										setHover(false);
+									}}
+									key={item.id}
+									className="movie_block"
+									style={{
+										...styles.movieBlock,
+									}}
+								>
+									<img
+										src={`${imageUrl}${item.backdrop_path}`}
+										className="category_poster"
+									/>
+								</div>
+							);
+						})}
+					</div>
+				</div>
+			)}
+		</>
+	);
 }
 
 export default Category;
--- src/Constants/styles.js
+++ src/Constants/styles.js
@@ -1,66 +1,65 @@
 import { useState, useEffect } from "react";
 
 function getWindowDimensions() {
-  const { innerWidth: width, innerHeight: height } = window;
-  return {
-    width,
-    height,
-  };
+	const { innerWidth: width, innerHeight: height } = window;
+	return {
+		width,
+		height,
+	};
 }
 
 function useWindowDimensions() {
-  const [windowDimensions, setWindowDimensions] = useState(
-    getWindowDimensions()
-  );
+	const [windowDimensions, setWindowDimensions] = useState(
+		getWindowDimensions()
+	);
 
-  useEffect(() => {
-    function handleResize() {
-      setWindowDimensions(getWindowDimensions());
-    }
+	useEffect(() => {
+		function handleResize() {
+			setWindowDimensions(getWindowDimensions());
+		}
 
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
+		window.addEventListener("resize", handleResize);
+		return () => window.removeEventListener("resize", handleResize);
+	}, []);
 
-  return windowDimensions;
+	return windowDimensions;
 }
 
 const styles = {
-  container: {
-    display: "flex",
-    flex: 1,
-    width: "100vw",
-    height: "100vh",
-    flexDirection: "column",
-    backgroundColor: "black",
-    padding: 0,
-  },
-  title: {
-    color: "white",
-  },
-  movieBlock: {
-    display: "flex",
-    flexDirection: "column",
-    zIndex: 10,
-    marginRight: 5,
-    //     height: "100px",
-    //     width: "100px",
-  },
-  posterInfo: {
-    display: "flex",
-    position: "absolute",
-  },
-  detailParentDiv: {
-    width: "100%",
-    backgroundColor: "#141414",
-    maxWidth: "850px",
-    borderRadius: 10,
-    overflow: "scroll",
-  },
+	container: {
+		display: "flex",
+		flex: 1,
+		width: "100vw",
+		height: "100vh",
+		flexDirection: "column",
+		backgroundColor: "black",
+		padding: 0,
+	},
+	title: {
+		color: "white",
+	},
+	movieBlock: {
+		display: "flex",
+		flexDirection: "column",
+		marginRight: 5,
+		//     height: "100px",
+		//     width: "100px",
+	},
+	posterInfo: {
+		display: "flex",
+		position: "absolute",
+	},
+	detailParentDiv: {
+		width: "100%",
+		backgroundColor: "#141414",
+		maxWidth: "850px",
+		borderRadius: 10,
+		overflow: "scroll",
+	},
 };
 
 const colors = {
-  primary: "#D7171E",
+	primary: "#D7171E",
 };
 
 export { styles, colors, useWindowDimensions };
